5f01574781b298fbf19b04a135a734b4942b8875
Stefan <stefan@wonka.(none)>
2011-02-09 22:24:03 +0200

    Added Atlys board port

---
+++ boards/xilinx/atlys/rtl/verilog/include/xilinx_ddr2_params.v
@@ -0,0 +1,12 @@
+   parameter C3_P0_MASK_SIZE           = 16;
+   parameter C3_P0_DATA_PORT_SIZE      = 128;
+   parameter DEBUG_EN                = 0;       
+   parameter C3_MEMCLK_PERIOD        = 3750;       
+   parameter C3_CALIB_SOFT_IP        = "TRUE";       
+   parameter C3_SIMULATION           = "FALSE";       
+   parameter C3_RST_ACT_LOW          = 0;       
+   parameter C3_INPUT_CLK_TYPE       = "SINGLE_ENDED";       
+   parameter C3_MEM_ADDR_ORDER       = "BANK_ROW_COLUMN";       
+   parameter C3_NUM_DQ_PINS          = 16;       
+   parameter C3_MEM_ADDR_WIDTH       = 13;       
+   parameter C3_MEM_BANKADDR_WIDTH   = 3;        
