fd230ebec21f6e3b353aa0cd47edd4e098d5b302
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2011-03-25 07:44:21 +0200

    Atlys: fixed preloading of DDR2 during simulation

--- boards/xilinx/atlys/bench/verilog/include/ddr2_model_preload.v
+++ boards/xilinx/atlys/bench/verilog/include/ddr2_model_preload.v
@@ -3,7 +3,7 @@
 
 // Wait until the DDR memory is initialised, and then magically
 // load it
-@(posedge dut.xilinx_ddr2_0.xilinx_ddr2_if0.phy_init_done);
+@(posedge dut.xilinx_ddr2_0.xilinx_ddr2_if0.ddr2_calib_done);
 //$display("%t: Loading DDR2",$time);
 
 $readmemh("sram.vmem", program_array);
@@ -11,46 +11,36 @@ $readmemh("sram.vmem", program_array);
 for(ram_ptr = 0 ; ram_ptr < 4096 ; ram_ptr = ram_ptr + 1)
   begin
 
-     // Construct the burst line, with every second word from where we
-     // started, and picking the correct half of the word with i%2
-     program_word_ptr = ram_ptr * 16 + (i/2) ; // Start on word0 or word1
+     // Construct the burst line
+     program_word_ptr = ram_ptr*4;
      tmp_program_word = program_array[program_word_ptr];
-     ddr2_ram_mem_line[15:0] = tmp_program_word[15 + ((i%2)*16):((i%2)*16)];
+     ddr2_ram_mem_line[31:0] = tmp_program_word;
 
-     program_word_ptr = program_word_ptr + 2;
-     tmp_program_word = program_array[program_word_ptr]; 
-     ddr2_ram_mem_line[31:16] = tmp_program_word[15 + ((i%2)*16):((i%2)*16)];
-     
-     program_word_ptr = program_word_ptr + 2;
+     program_word_ptr = program_word_ptr + 1;
      tmp_program_word = program_array[program_word_ptr];
-     ddr2_ram_mem_line[47:32] = tmp_program_word[15 + ((i%2)*16):((i%2)*16)];
+     ddr2_ram_mem_line[63:32] = tmp_program_word;
      
-     program_word_ptr = program_word_ptr + 2;
-     tmp_program_word = program_array[program_word_ptr];
-     ddr2_ram_mem_line[63:48] = tmp_program_word[15 + ((i%2)*16):((i%2)*16)];
      
-     program_word_ptr = program_word_ptr + 2;
+     program_word_ptr = program_word_ptr + 1;
      tmp_program_word = program_array[program_word_ptr];
-     ddr2_ram_mem_line[79:64] = tmp_program_word[15 + ((i%2)*16):((i%2)*16)];
+     ddr2_ram_mem_line[95:64] = tmp_program_word;
      
-     program_word_ptr = program_word_ptr + 2;
-     tmp_program_word = program_array[program_word_ptr];
-     ddr2_ram_mem_line[95:80] = tmp_program_word[15 + ((i%2)*16):((i%2)*16)];
      
-     program_word_ptr = program_word_ptr + 2;
+     program_word_ptr = program_word_ptr + 1;
      tmp_program_word = program_array[program_word_ptr];
-     ddr2_ram_mem_line[111:96] = tmp_program_word[15 + ((i%2)*16):((i%2)*16)];
-     
-     program_word_ptr = program_word_ptr + 2;
-     tmp_program_word = program_array[program_word_ptr];
-     ddr2_ram_mem_line[127:112] = tmp_program_word[15 + ((i%2)*16):((i%2)*16)];
+     ddr2_ram_mem_line[127:96] = tmp_program_word;
      
      // Put this assembled line into the RAM using its memory writing TASK
-     u_mem0.memory_write(2'b00,ram_ptr[19:7],
-			 {ram_ptr[6:0],3'b000},ddr2_ram_mem_line);
-     
+     if (C3_MEM_ADDR_ORDER == "BANK_ROW_COLUMN") begin
+	u_mem0.memory_write(2'b00,ram_ptr[19:7],
+			   {ram_ptr[6:0],3'b000},ddr2_ram_mem_line);
+     end else if (C3_MEM_ADDR_ORDER == "ROW_BANK_COLUMN") begin
+	u_mem0.memory_write(ram_ptr[8:7],{2'b00,ram_ptr[19:9]},
+			   {ram_ptr[6:0],3'b000},ddr2_ram_mem_line);
+	
+     end
      //$display("Writing 0x%h, ramline=%d",ddr2_ram_mem_line, ram_ptr);
      
   end // for (ram_ptr = 0 ; ram_ptr < ...
-$display("(%t) * DDR2 RAM %1d preloaded",$time, i);
+$display("(%t) * DDR2 RAM preloaded",$time);
 
