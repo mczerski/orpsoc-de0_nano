1994b2c0a3a43bae6eb04183e69b9b57df5b3875
Marek Czerski <ma.czerski@gmail.com>
2012-11-08 01:06:54 +0100

    deleted unwanted tracked files and change in pin assignment for UART0

--- boards/altera/de0_nano/syn/quartus/tcl/UART0_pin_assignments.tcl
+++ boards/altera/de0_nano/syn/quartus/tcl/UART0_pin_assignments.tcl
@@ -1,4 +1,4 @@
-set_location_assignment PIN_C16 -to uart0_srx_pad_i
+set_location_assignment PIN_D8 -to uart0_srx_pad_i
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart0_srx_pad_i
-set_location_assignment PIN_D15 -to uart0_stx_pad_o
+set_location_assignment PIN_F8 -to uart0_stx_pad_o
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart0_stx_pad_o
